259f6e842e2ce7a4f4d3c2bf961c97b2815d2533
Priyanshu Yadav <priyanshurajyadav56@gmail.com>
2026-07-10 15:40:49 +0530

    Separate blog routes and configure EJS layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const path = require("path");
 const engine= require("ejs-mate")
+const blogRoutes = require("./routes/blog");
 
 const app= express();
 app.engine("ejs" , engine);
@@ -9,10 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const port=8080;
 
-app.get("/" , (req ,res)=>{
-    res.render("index.ejs")
-});
-
+app.use("/", blogRoutes);
 app.listen(port ,()=>{
     console.log("port is listening on " + port)
 });
