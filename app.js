8aca3ac0d8212c90ba57479902649fa0195cae00
Priyanshu Yadav <priyanshurajyadav56@gmail.com>
2026-07-12 16:02:33 +0530

    Connect MongoDB with Mongoose

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express= require("express");
 const path = require("path");
 const engine= require("ejs-mate")
 const blogRoutes = require("./routes/blogs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app= express();
 app.engine("ejs" , engine);
@@ -13,6 +16,19 @@ const port=8080;
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/blogs", blogRoutes);
+const dbUrl = process.env.ATLASDB_URL;
+
+main()
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
 app.listen(port ,()=>{
     console.log("port is listening on " + port)
 });
